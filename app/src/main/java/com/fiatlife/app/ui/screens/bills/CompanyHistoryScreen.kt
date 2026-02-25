f466abb335b67ee700ff4904a2b142b0f2de381c
homebrew-gh <homelog_contact@protonmail.com>
2026-02-25 10:58:47 -0500

    Add experimental API opt-in for CompanyHistoryScreen
    - Added `@OptIn(ExperimentalMaterial3Api::class)` annotation to `CompanyHistoryScreen` to enable the use of experimental Material3 components.
    - This change aims to leverage new UI features while ensuring compatibility with the latest Material design guidelines.

--- app/src/main/java/com/fiatlife/app/ui/screens/bills/CompanyHistoryScreen.kt
+++ app/src/main/java/com/fiatlife/app/ui/screens/bills/CompanyHistoryScreen.kt
@@ -37,6 +37,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CompanyHistoryScreen(
     navController: NavController,
